7b4bc7efbe49fe405093c74885c7fec22ec5863b
Dave Powers <djpowers89@gmail.com>
2018-12-15 13:34:13 -0500

    Add pry and sinatra reloader

--- Gemfile
+++ Gemfile
@@ -1,2 +1,6 @@
 source 'https://rubygems.org'
 gem 'sinatra'
+
+group :development, :test do
+  gem 'pry'
+end
--- myapp.rb
+++ myapp.rb
@@ -1,4 +1,9 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+configure :development, :test do
+  require 'pry'
+end
 
 get '/' do
   'Hello world!'
