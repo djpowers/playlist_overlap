d4a1e929289c300821e4f87a0a11eda918249fa7
Dave Powers <djpowers89@gmail.com>
2018-12-16 15:51:57 -0500

    Fetch playlists and parse out common tracks
    - split URL params into individual IDs
    - fetch tracks from all playlists and add to single array
    - determine duplicated track IDs
    - construct hash with each track ID and number of occurrences, sorted by
    most occurences
    - group together tracks based on duplicated track IDs, and replace ID
    with song name and artist

--- myapp.rb
+++ myapp.rb
@@ -9,4 +9,28 @@ end
 
 get '/' do
   RSpotify.authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
+  playlist_ids = params[:playlist_ids].split
+
+  tracks = []
+  playlist_ids.each do |playlist_id|
+    playlist = RSpotify::Playlist.find(nil, playlist_id)
+    tracks += playlist.tracks
+  end
+  tracks.flatten
+
+  track_ids = tracks.map(&:id)
+  duplicated_track_ids = track_ids.select { |id| track_ids.count(id) > 1 }
+
+  track_id_counts = Hash.new(0)
+  duplicated_track_ids.each { |track| track_id_counts[track] += 1 }
+  sorted_track_id_counts = track_id_counts
+                           .sort_by { |_key, value| value }
+                           .reverse.to_h
+
+  shared_tracks = tracks.select { |track| duplicated_track_ids.include?(track.id) }.uniq(&:id)
+  @display_tracks = {}
+  sorted_track_id_counts.each do |key, value|
+    track = shared_tracks.detect { |shared_track| shared_track.id == key }
+    @display_tracks["'#{track.name}' by #{track.artists.first.name}"] = value
+  end
 end
