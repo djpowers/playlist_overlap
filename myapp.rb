08ad9673d5e4714c3e99741d0fca817bba9f93d7
Dave Powers <djpowers89@gmail.com>
2018-12-16 20:58:09 -0500

    Parse out artist counts as we do tracks
    - get unique artists from each playlist
    - find common artists between playlists
    - build hash based on artists counts
    - sort hash by occurrences

--- myapp.rb
+++ myapp.rb
@@ -13,14 +13,19 @@ get '/' do
   playlist_ids = params[:playlist_ids].split
 
   tracks = []
+  artists = []
   playlist_ids.each do |playlist_id|
     playlist = RSpotify::Playlist.find(nil, playlist_id)
     tracks += playlist.tracks
+
+    artists += playlist.tracks.map { |track| track.artists.first.name }.uniq
   end
   tracks.flatten
 
   track_ids = tracks.map(&:id)
   duplicated_track_ids = track_ids.select { |id| track_ids.count(id) > 1 }
+
+  common_artists = artists.select { |artist| artists.count(artist) > 1 }
 
   track_id_counts = Hash.new(0)
   duplicated_track_ids.each { |track| track_id_counts[track] += 1 }
@@ -28,12 +33,17 @@ get '/' do
                            .sort_by { |_key, value| value }
                            .reverse.to_h
 
+  artists_count = Hash.new(0)
+  common_artists.each { |artist| artists_count[artist] += 1 }
+
   shared_tracks = tracks.select { |track| duplicated_track_ids.include?(track.id) }.uniq(&:id)
   @display_tracks = {}
   sorted_track_id_counts.each do |key, value|
     track = shared_tracks.detect { |shared_track| shared_track.id == key }
     @display_tracks["\"#{track.name}\" by #{track.artists.first.name}"] = value
   end
+
+  @artists_count = artists_count.sort_by { |_key, value| value }.reverse.to_h
 
   erb :index
 end
